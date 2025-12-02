bb2fdd9275e001811b26b663798acedd651fbfaf
miku3ln <kalexmiguelalba@gmail.com>
2025-12-02 12:24:25 -0500

    a

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.util.Properties
+import java.io.FileInputStream
+val keystoreProperties = Properties().apply {
+    val keystorePropertiesFile = rootProject.file("android/key.properties")
+    if (keystorePropertiesFile.exists()) {
+        load(keystorePropertiesFile.inputStream())
+    }
+}
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +14,7 @@ plugins {
 }
 
 android {
-    namespace = "com.meetclic.meetclic_movile.meetclic_app"
+    namespace = "com.meetclic.meetclic_movile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,20 +29,36 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.meetclic.meetclic_movile.meetclic_app"
+        applicationId = "com.meetclic.meetclic_movile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
+       // minSdk = flutter.minSdkVersion TODO NEW CHANGE
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    signingConfigs {
+        create("release") {
+            // ruta relativa a android/app
+            storeFile = file("keystore/meetclic.keystore")
+            storePassword = "st+-963852"
+            keyAlias = "meetclic_key"
+            keyPassword = "st+-963852"
+        }
+    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            getByName("release") {
+                // Por ahora firmas con debug, solo para pruebas
+                signingConfig = signingConfigs.getByName("debug")
+
+                // TODO FIX ERROR RELEASE Desactivar R8 y shrink para evitar el error de Sceneform
+                isMinifyEnabled = false
+                isShrinkResources = false
+            }
         }
     }
 }
--- android/app/src/main/java/com/meetclic/meetclic_movile/meetclic_app/MainActivity.java
+++ android/app/src/main/java/com/meetclic/meetclic_movile/meetclic_app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.meetclic.meetclic_movile.meetclic_app;
+package com.meetclic.meetclic_movile;
 
 import io.flutter.embedding.android.FlutterActivity;
 
